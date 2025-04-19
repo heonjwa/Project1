c12b0f3c619d32c92a7d9b2840ef32bb86e071b6
Sheepca <mtrien01@yahoo.com>
2025-04-18 23:40:22 -0700

    done with part b?

--- part2/server.py
+++ part2/server.py
@@ -6,7 +6,7 @@ import threading
 import logging
 
 TIMEOUT = 3
-STUDENT_ID = "056"  # Replace with your own
+STUDENT_ID = "187"  # Replace with your own
 
 logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s')
 
@@ -32,9 +32,9 @@ class ClientHandler(threading.Thread):
         self.length = length
         self.udp_port = udp_port
         self.tcp_port = random.randint(20000, 30000)
-        self.secretB = random.randint(1000, 9999)
-        self.secretC = random.randint(1000, 9999)
-        self.secretD = random.randint(1000, 9999)
+        self.secret_b = random.randint(1000, 9999)
+        self.secret_c = random.randint(1000, 9999)
+        self.secret_d = random.randint(1000, 9999)
         self.server_name = server_name
 
     def run(self):
@@ -63,6 +63,7 @@ class ClientHandler(threading.Thread):
             try:
                 data, addr = udp_socket.recvfrom(1024)
                 udp_socket.settimeout(TIMEOUT)
+                logging.info(f'Client {addr} | Stage B recv {data}')
                 # TODO: Receive self.num packets, verify IDs and len
                 # Verify length = length + 4
                 # Verify header
@@ -89,8 +90,9 @@ class ClientHandler(threading.Thread):
                     header = create_header(4, self.secret_a, 2)
                     ack_payload = struct.pack("!I", curr_packet) 
                     ack_packet = header + ack_payload
-                    udp_socket.sendto(ack_packet, self.client_addr)
-                    logging.info(f"Sent ACK for packet {curr_packet} to {self.client_addr}")
+                    udp_socket.sendto(ack_packet, addr)
+                    logging.info(f"Sent ACK for packet {curr_packet}")
+                    curr_packet += 1
 
             except socket.timeout:
                 logging.warning("Main server timed out waiting for packets.")
@@ -100,7 +102,7 @@ class ClientHandler(threading.Thread):
                 break
             
         header = create_header(8, self.secret_b, 2)
-        ack_payload = struct.pack("!II", self.tcp_port, self.secretB)
+        ack_payload = struct.pack("!II", self.tcp_port, self.secret_b)
         ack_packet = header + ack_payload
         ack_packet = pad_to_4_byte_boundary(ack_packet)
         udp_socket.sendto(ack_packet, self.client_addr)
